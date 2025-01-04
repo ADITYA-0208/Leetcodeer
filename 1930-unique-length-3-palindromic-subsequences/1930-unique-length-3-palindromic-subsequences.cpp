caff68d7308d990db8f7c6787e08c51f12e9eea9
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-01-04 01:05:30 -0800

    Time: 162 ms (69.02%), Space: 19 MB (15.95%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,27 +1,26 @@
+//learned form chatgpt 
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<vector<char>> ans; 
-        vector<char> p; 
-        generateSubsets(ans, p, s, 0);
-        sort(ans.begin(), ans.end());
-        auto it = unique(ans.begin(), ans.end());
-        ans.erase(it, ans.end());
-        return ans.size();
-    }
-
-    void generateSubsets(vector<vector<char>>& ans, vector<char>& p, string& s, int i) {
-        if (p.size() == 3 && p[0] == p[2]) { 
-            ans.push_back(p);
-            return;
+        vector<int>first(26,-1),last(26,-1);
+        unordered_set<string> un;
+        for(int i=0;i<s.size();i++){
+            if(first[s[i]-'a']==-1){
+                first[s[i]-'a']=i;
+            }
+            last[s[i]-'a']=i;
         }
-
-        if (i >= s.size() ) { 
-            return;
+        for(int i=0;i<26;i++){
+            if(first[i]!=-1 || last[i]>first[i]){
+                unordered_set<char> middle;
+                for(int j=first[i]+1;j<last[i];j++){
+                    middle.insert(s[j]);
+                }
+                for(auto it :middle){
+                    un.insert(string(1, 'a' + i) + it + string(1, 'a' + i));
+                }
+            }
         }
-        p.push_back(s[i]);
-        generateSubsets(ans, p, s, i + 1);
-        p.pop_back();
-        generateSubsets(ans, p, s, i + 1);
+        return un.size();
     }
 };
