9a1aacfbb7163ec1ec239af84bc78f4caa37d71c
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-01-11 01:32:53 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>ans(n+1,0);
+        for(int i=0;i<trust.size();i++){
+                ans[trust[i][1]]++;
+        }
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]==n-1){
+                 for(int j=0;j<trust.size();j++){
+                    if(trust[j][0]==i)  return -1;  
+                 }
+                 return i;
+            }
+        }
+        return -1;
+    }
+};
