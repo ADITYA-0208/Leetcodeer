83cf4c429a1ebcc1f71ba1c1708993cb9378752d
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-01-21 09:56:24 -0800

    Time: 0 ms (100%), Space: 15.1 MB (88.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = findMin(nums);
-        if(n==2){
-            if(nums[0]==target) return 0;
-            else if(nums[1]==target)    return 1;
-            else return -1;
-        }
-        int h = (l + n) % n;
-        int p = n - l;
-        int he = (h + p) % n;
-        int le = (l + p) % n;
-
-        while (he <= le) {
-            int mide = le + (he - le) / 2;
-            int mid = (mide - p + n) % n; 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                he = mid + 1;
-            } else {
-                le = mid - 1;
-            }
-        }
-        return -1; 
-    }
-
-    int findMin(vector<int>& nums) {
         int low = 0, high = nums.size() - 1;
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid;
+        while (low<=high) {
+            int mid =  (high +low) / 2;
+            if(nums[mid]==target)    return mid;
+            else{
+                if(nums[low]==nums[high] &&nums[mid]==nums[high]){
+                    low++;
+                    high--;
+                }
+            else if(nums[low]<= nums[mid]){
+                if(nums[low]<=target && target<=nums[mid] ) high=mid-1;
+                else  low=mid+1;
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high])  low=mid+1;
+                else    high=mid-1;
+            }
             }
         }
 
-        return low;
+        return -1;
     }
 };
