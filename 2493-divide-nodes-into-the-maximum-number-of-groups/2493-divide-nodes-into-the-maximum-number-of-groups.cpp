39b8a28da7bc6368f6b07b3a095598f339e7ef58
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-01-30 16:01:07 +0530

    Time: 128 ms (79.83%), Space: 56.6 MB (27.73%) - LeetHub

--- 2493-divide-nodes-into-the-maximum-number-of-groups/2493-divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2493-divide-nodes-into-the-maximum-number-of-groups/2493-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -7,24 +7,57 @@ public:
             graph[edge[1]].push_back(edge[0]);
         }
 
-        if (!isBipartite(n, graph)) return -1;
-
-        vector<int> visited(n + 1, 0);
+        vector<int> globalVisited(n + 1, 0); 
         int mx = 0;
 
         for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                mx = max(mx, levels(graph, i, visited));
+            if (!globalVisited[i]) {
+                vector<int> component;
+                if (!isBipartite(i, graph, globalVisited, component)) return -1;
+                int maxLevels = 0;
+                for (int node : component) {
+                    vector<int> visited(n + 1, 0);
+                    maxLevels = max(maxLevels, levels(graph, node, visited));
+                }
+
+                mx += maxLevels;
             }
         }
         return mx;
     }
 
-    int levels(vector<vector<int>>& graph, int i, vector<int> &visited) {
+    bool isBipartite(int start, vector<vector<int>>& graph, vector<int>& globalVisited, vector<int>& component) {
+        queue<int> q;
+        vector<int> color(graph.size(), -1);
+        
+        q.push(start);
+        color[start] = 0;
+        globalVisited[start] = 1;
+        component.push_back(start);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int v : graph[u]) {
+                if (color[v] == -1) { 
+                    color[v] = 1 - color[u];  
+                    globalVisited[v] = 1;
+                    q.push(v);
+                    component.push_back(v);
+                } else if (color[v] == color[u]) {
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+
+    int levels(vector<vector<int>>& graph, int start, vector<int> &visited) {
         queue<int> q;
         int total = 0;
-        q.push(i);
-        visited[i] = 1;
+        q.push(start);
+        visited[start] = 1;
         
         while (!q.empty()) {
             int size = q.size();
@@ -40,33 +73,6 @@ public:
             }
             total++;
         }
-        return total+1;
-    }
-
-    bool isBipartite(int V, vector<vector<int>> &adj) {
-        vector<int> color(V + 1, -1);
-        queue<int> q;
-        
-        for (int i = 1; i <= V; i++) {
-            if (color[i] == -1) {
-                color[i] = 0;
-                q.push(i);
-                
-                while (!q.empty()) {
-                    int u = q.front();
-                    q.pop();
-                    
-                    for (auto &v : adj[u]) {
-                        if (color[v] == -1) {
-                            color[v] = 1 - color[u];
-                            q.push(v);
-                        } else if (color[v] == color[u]) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
+        return total;  
     }
 };
