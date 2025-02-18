10b8762fd75eb5464fbff179d4e3cfa8094045f9
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-02-18 16:47:44 +0530

    Time: 0 ms (100%), Space: 11.5 MB (68.96%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<bool> used(nums.size(), false);
-        vector<int> s;
         vector<vector<int>> ans;
-        int ind = 0;  
-        fucc(used, s, ans, nums, ind);
+        vector<int> s;
+        vector<bool> used(nums.size(), false);
+        
+        sort(nums.begin(), nums.end());  
+        
+        backtrack(nums, used, s, ans);
         return ans;
     }
-
-    void fucc(vector<bool> used, vector<int>& s, vector<vector<int>>& ans, vector<int>& nums, int ind) {
+    
+    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& s, vector<vector<int>>& ans) {
         if (s.size() == nums.size()) {
             ans.push_back(s);
             return;
         }
+        
         for (int i = 0; i < nums.size(); i++) {
             if (used[i]) continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue; 
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue; // Skip duplicates
             
             used[i] = true;
             s.push_back(nums[i]);
-            fucc(used,s,ans,nums,ind);
+            backtrack(nums, used, s, ans);
             s.pop_back();
             used[i] = false;
         }
