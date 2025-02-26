1d96942cb63d5ca90bed287957924f9eed4409d4
ADITYA-0208 <142163734+ADITYA-0208@users.noreply.github.com>
2025-02-26 12:41:04 +0530

    Time: 4 ms (14.13%), Space: 45.1 MB (51.09%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -5,21 +5,21 @@ public:
     }
     int maxSubarraySum(vector<int> &arr) {
         int res = arr[0];
-        int maxEnding = arr[0];
+        int maxi = arr[0];
 
         for (int i = 1; i < arr.size(); i++) {
-            maxEnding = max(maxEnding + arr[i], arr[i]);
-            res = max(res, maxEnding);
+            maxi= max(maxi + arr[i], arr[i]);
+            res = max(res, maxi);
         }
         return res;
     }
     int minSubarraySum(vector<int> &arr) {
         int res = arr[0];
-        int maxEnding = arr[0];
+        int maxi = arr[0];
 
         for (int i = 1; i < arr.size(); i++) {
-            maxEnding = min(maxEnding + arr[i], arr[i]);
-            res = min(res, maxEnding);
+            maxi = min(maxi + arr[i], arr[i]);
+            res = min(res, maxi);
         }
         return res;
     }
